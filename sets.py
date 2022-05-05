f2fe5bd537074781a418c80871e9dfbf70710fd2
albertobd85 <albertobd85@gmail.com>
2022-05-05 23:09:39 +0200

    push

--- sets.py
+++ sets.py
@@ -1,6 +1,6 @@
-#Using python to manipulate sets
+# Using python to manipulate sets
 
-'''
+"""
 Python knows a number of compound data types, 
 used to group together other values.
 They are:
@@ -32,28 +32,28 @@ symmetric_difference() Returns a set with the symmetric differences of two sets
 symmetric_difference_update() Inserts the symmetric differences from this set and another
 union() Return a set containing the union of sets
 update() Update the set with another set, or any other iterable
-'''
+"""
 
-#The basics
-basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
-print(basket)# show that duplicates have been removed
+# The basics
+basket = {"apple", "orange", "apple", "pear", "orange", "banana"}
+print(basket)  # show that duplicates have been removed
 
-'orange' in basket # fast membership testing
-'crabgrass' in basket
+"orange" in basket  # fast membership testing
+"crabgrass" in basket
 
 # Demonstrate set operations on unique letters from two words
-a = set('abracadabra')
-b = set('alacazam')
-a # unique letters in a
-a - b # letters in a but not in b
-a | b # letters in a or b or both
-a & b # letters in both a and b
-a ^ b # letters in a or b but not both
+a = set("abracadabra")
+b = set("alacazam")
+a  # unique letters in a
+a - b  # letters in a but not in b
+a | b  # letters in a or b or both
+a & b  # letters in both a and b
+a ^ b  # letters in a or b but not both
 
 
-#Set comprehension
-a = {x for x in 'abracadabra' if x not in 'abc'}
+# Set comprehension
+a = {x for x in "abracadabra" if x not in "abc"}
 
-#built-in function set()
-x = set(('bobby','bobby', 'at', 'didcoding','dot', 'com')) # creates a set object
+# built-in function set()
+x = set(("bobby", "bobby", "at", "didcoding", "dot", "com"))  # creates a set object
 x
